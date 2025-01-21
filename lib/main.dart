2d194da271866b10ba1d0177dd337df9ca009a24
danielwidhiarto <danielwidhiarto@gmail.com>
2025-01-21 10:07:15 +0700

    add firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
+import 'package:asklab/page/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'page/LoginPage.dart'; // Import the LoginPage
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Firebase initialization
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(), // Set LoginPage as the default page
+      title: 'AskLab',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LoginPage(),
     );
   }
 }
